bd756964eba11635faf9a9404238aecd8526443f
Alex Kawa <alex.kawaschinski@gmail.com>
2021-03-29 23:42:52 +0200

    Add first version of App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "./utils/getTheme";
+import { IntlProvider } from "react-intl";
+import { flattenMessages } from "./utils/common";
+import de from "./locales/de";
+import backgroundImg from "./assets/landing-page.png";
 
-function App() {
+import HeaderBar from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Wizard from "./components/Wizard";
+
+const Wrapper = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+  max-height: 100vh;
+  background-color: ${(props) => props.theme.background};
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 35px;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex: 1;
+  padding-right: 10px;
+`;
+
+const Background = styled.div`
+  position: relative;
+  background-image: url(${backgroundImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  flex: 1;
+  width: 100%;
+  border-radius: 10px;
+`;
+
+const Controls = styled.section`
+  background-color: ${(props) => props.theme.backgroundSecondary};
+  height: 60px;
+
+  @media (${(props) => props.theme.tabletScreen}) {
+    display: none;
+  }
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider locale="de" messages={flattenMessages(de)}>
+      <ThemeProvider theme={theme}>
+        <Wrapper>
+          <HeaderBar />
+          <Content>
+            <Sidebar />
+            <Background>
+              <Wizard />
+            </Background>
+          </Content>
+          <Controls />
+        </Wrapper>
+      </ThemeProvider>
+    </IntlProvider>
   );
-}
+};
 
 export default App;
