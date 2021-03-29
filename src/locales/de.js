f3d25b73c83f051d6b491f0d68f28a4780b4ff04
Alex Kawa <alex.kawaschinski@gmail.com>
2021-03-29 23:41:22 +0200

    Add de locale

---
+++ src/locales/de.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line
+export default {
+  menu: {
+    menu: "Menü",
+    contact: "Kontakt",
+    projectBy: "Ein Projekt von: Pr1mus",
+    presentedBy: "Präsentiert von: Grossman & Berger",
+  },
+  wizard: {
+    welcome: "Willkommen zu dem Sellin Kurhaus Project",
+    help: "Können wir Ihnen beim suchen helfen?",
+  },
+};
