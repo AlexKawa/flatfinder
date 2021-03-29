754d2de2fa9b82d7ed6b1a73dbb2437c334f447e
Alex Kawa <alex.kawaschinski@gmail.com>
2021-03-29 23:40:14 +0200

    Add getTheme and common utils

---
+++ src/utils/common.tsx
@@ -0,0 +1,24 @@
+interface IDictionary {
+  [id: string]: string | IDictionary;
+}
+
+export const flattenMessages = (
+  nestedMessages: IDictionary,
+  prefix: string = ""
+) => {
+  if (nestedMessages === null) {
+    return {};
+  }
+  return Object.keys(nestedMessages).reduce((messages, key) => {
+    const value = nestedMessages[key];
+    const prefixedKey = prefix ? `${prefix}.${key}` : key;
+
+    if (typeof value === "string") {
+      Object.assign(messages, { [prefixedKey]: value });
+    } else {
+      Object.assign(messages, flattenMessages(value, prefixedKey));
+    }
+
+    return messages;
+  }, {});
+};
---
+++ src/utils/getTheme.tsx
@@ -0,0 +1,50 @@
+const getTheme = () => {
+  const colors = {
+    primary: "#1959FD",
+    secondary: "#000000",
+    background: "#F0F0F0",
+    backgroundSecondary: "#FFFFFF",
+    backgroundInvert: "#DDD4CC",
+    success: "#33CF85",
+    danger: "#CE5454",
+    medium: "#7F8188",
+    dark: "#343438",
+    black: "#000000",
+    light: "#54504E",
+    blurryDark: "rgba(29, 30, 34, 0.7)",
+    testPrimary: "#1959FD",
+    textBody: "#161616",
+    textSecondary: "#FFFFFF",
+    textTertiary: "#848B90",
+    textMedium: "#7D7D7D",
+    textHighlight: "#FFFFFF",
+    colorDifference: "rgba(25, 89, 253, 0.1)",
+    tilesDarken: "rgba(218, 215, 215, 0.2)",
+    succesDark: "#EFF6E9",
+    reservedDark: "#F6F2E9",
+  };
+
+  const fontSize = "14px";
+  const fonts = {
+    fontSizes: {
+      miniscul: "8px",
+      tiny: "10px",
+      small: "12px",
+      default: fontSize,
+      bigger: "24px",
+    },
+  };
+
+  const screens = {
+    tabletScreen: "min-width: 641px",
+    desktopScreen: "min-width: 1281px",
+  };
+
+  return {
+    ...colors,
+    ...fonts,
+    ...screens,
+  };
+};
+
+export default getTheme;
