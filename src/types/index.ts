815734a1c067de189fc33784a24c6602f7b4ec18
Alex Kawa <alex.kawaschinski@gmail.com>
2021-04-07 00:18:52 +0200

    Add type file

---
+++ src/types/index.ts
@@ -0,0 +1,21 @@
+export interface IUnit {
+  id: string;
+  int_id: string;
+  room: number;
+  type: string;
+  name: string;
+  description: null;
+  status: string;
+  room_count: number;
+  floor: string;
+  size_total: number;
+  tag: null;
+  category: null;
+  kind: string;
+  best_view: string;
+  best_view_image: string;
+  sold: boolean;
+  price: number;
+  price_toal: number;
+  images: string[];
+}
