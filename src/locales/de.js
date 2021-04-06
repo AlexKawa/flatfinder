90ec6288f7686eaa6759d6b3feefee95ccda6788
Alex Kawa <alex.kawaschinski@gmail.com>
2021-04-06 15:23:53 +0200

    Add new translations

--- src/locales/de.js
+++ src/locales/de.js
@@ -3,11 +3,25 @@ export default {
   menu: {
     menu: "Menü",
     contact: "Kontakt",
-    projectBy: "Ein Projekt von: Pr1mus",
-    presentedBy: "Präsentiert von: Grossman & Berger",
+    filters: "Filter",
+    projectBy: "Ein Projekt von: PROJECTNAME",
+    presentedBy: "Präsentiert von: AGENTS",
+    hide: "Verbergen",
   },
   wizard: {
     welcome: "Willkommen zu dem Sellin Kurhaus Project",
     help: "Können wir Ihnen beim suchen helfen?",
+  },
+  attributes: {
+    rooms: "Räume",
+    price: "Preis",
+    type: "Apartment-Typ",
+    surface: "Grundfläche",
+    availability: "Verfügbarkeit",
+    available: "Verfügbar",
+    reserved: "Reserviert",
+  },
+  sidebar: {
+    goBack: "Zurück zur Übersicht",
   },
 };
