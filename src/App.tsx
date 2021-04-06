825dccb2925fd14ff6b29343e3b4b6873c40fc3e
Alex Kawa <alex.kawaschinski@gmail.com>
2021-04-07 00:17:38 +0200

    Adjust and connect index and App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,25 @@
 import React from "react";
-import styled, { ThemeProvider } from "styled-components";
-import theme from "./utils/getTheme";
+import { ThemeProvider } from "styled-components";
+import { withRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 import { IntlProvider } from "react-intl";
-import { flattenMessages } from "./utils/common";
-import de from "./locales/de";
-import backgroundImg from "./assets/landing-page.png";
 
-import HeaderBar from "./components/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Wizard from "./components/Wizard";
+import de from "locales/de";
+import routes from "routes";
+import { store } from "store/store";
+import theme from "utils/getTheme";
+import { flattenMessages } from "utils/common";
 
-const Wrapper = styled.div`
-  min-width: 100vw;
-  min-height: 100vh;
-  max-height: 100vh;
-  background-color: ${(props) => props.theme.background};
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  padding-bottom: 35px;
-`;
+import Flatfinder from "components/Flatfinder";
 
-const Content = styled.div`
-  display: flex;
-  flex: 1;
-  padding-right: 10px;
-`;
-
-const Background = styled.div`
-  position: relative;
-  background-image: url(${backgroundImg});
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  flex: 1;
-  width: 100%;
-  border-radius: 10px;
-`;
-
-const Controls = styled.section`
-  background-color: ${(props) => props.theme.backgroundSecondary};
-  height: 60px;
-
-  @media (${(props) => props.theme.tabletScreen}) {
-    display: none;
-  }
-`;
-
-const App = () => {
-  return (
+const App = () => (
+  <Provider store={store}>
     <IntlProvider locale="de" messages={flattenMessages(de)}>
       <ThemeProvider theme={theme}>
-        <Wrapper>
-          <HeaderBar />
-          <Content>
-            <Sidebar />
-            <Background>
-              <Wizard />
-            </Background>
-          </Content>
-          <Controls />
-        </Wrapper>
+        <Route path={routes.object} component={Flatfinder} />
       </ThemeProvider>
     </IntlProvider>
-  );
-};
+  </Provider>
+);
 
-export default App;
+export default withRouter(App);
